fb64428fd32fb34c68d2fc0b5f820aa3371ebffd
j23039885 <j23039885@student.newinti.edu.my>
2025-07-15 14:12:57 +0800

    Update upload.js

--- upload.js
+++ upload.js
@@ -11,13 +11,13 @@ const menuData = JSON.parse(
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyBagIcddt7BjkYsvC33BqDQfMzsFdjy5D0",
-  authDomain: "yuanfong-84448.firebaseapp.com",
-  projectId: "yuanfong-84448",
-  storageBucket: "yuanfong-84448.firebasestorage.app",
-  messagingSenderId: "743419868803",
-  appId: "1:743419868803:web:b0c2548633927bd302cdb6",
-  measurementId: "G-HCBLT09FXN"
+  apiKey: "reeee",
+  authDomain: "ree",
+  projectId: "reee",
+  storageBucket: "reee",
+  messagingSenderId: "reee",
+  appId: "ree",
+  measurementId: "ree"
 };
 
 // Initialize Firebase
